741e2dda666267684ca5ed705056bb5513ee4fa3
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-04-26 15:29:08 +0100

    Add: cutting-ribbons - submission-0

---
+++ Data Structures & Algorithms/cutting-ribbons/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        left = 0 
+        right = max(ribbons)
+
+        while left < right:
+            middle = (left + right + 1) // 2
+            if self.is_possible(middle, ribbons, k):
+                left = middle
+            else:
+                right = middle - 1
+        return left 
+    def is_possible(self, x: int, ribbons: list[int], k: int) -> bool:
+        total_ribbons = 0
+        for ribbon in ribbons:
+            total_ribbons += ribbon // x
+            if total_ribbons >= k:
+                return True
+        return False
