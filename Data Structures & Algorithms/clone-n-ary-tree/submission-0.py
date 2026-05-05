ed7ffe590d5c61e3a1cd4b9d1a3544ca43eb6121
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-05-05 19:12:16 +0100

    Add: clone-n-ary-tree - submission-0

---
+++ Data Structures & Algorithms/clone-n-ary-tree/submission-0.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return root
+
+        new_root = Node(root.val)
+        # Starting point to kick off the DFS visits.
+        stack = [(root, new_root)]
+
+        while stack:
+            old_node, new_node = stack.pop()
+            for child_node in old_node.children:
+                new_child_node = Node(child_node.val)
+
+                # Make a copy for each child node.
+                new_node.children.append(new_child_node)
+
+                # Schedule a visit to copy the child nodes of each child node.
+                stack.append((child_node, new_child_node))
+
+        return new_root
