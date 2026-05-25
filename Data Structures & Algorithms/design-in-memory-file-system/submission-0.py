b71692263fd98851d3d9cdc41c8d7bbff36a1fb5
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-05-25 21:41:39 +0100

    Add: design-in-memory-file-system - submission-0

---
+++ Data Structures & Algorithms/design-in-memory-file-system/submission-0.py
@@ -0,0 +1,66 @@
+class FileSystem:
+    class Dir:
+        def __init__(self):
+            self.dirs = {}
+            self.files = {}
+    
+    def __init__(self):
+        self.root = self.Dir()
+    
+    def ls(self, path: str) -> List[str]:
+        t = self.root
+        files = []
+        
+        if path != "/":
+            d = path.split("/")
+
+            for i in range(1, len(d) - 1):
+                t = t.dirs[d[i]]
+            
+            if d[-1] in t.files:
+                files.append(d[-1])
+                return files
+            else:
+                t = t.dirs[d[-1]]
+        
+        files.extend(t.dirs.keys())
+        files.extend(t.files.keys())
+        files.sort()
+        return files
+    
+    def mkdir(self, path: str) -> None:
+        t = self.root
+        d = path.split("/")
+        
+        for i in range(1, len(d)):
+            if d[i] not in t.dirs:
+                t.dirs[d[i]] = self.Dir()
+            t = t.dirs[d[i]]
+    
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        t = self.root
+        d = filePath.split("/")
+        
+        for i in range(1, len(d) - 1):
+            t = t.dirs[d[i]]
+        
+        if d[-1] not in t.files:
+            t.files[d[-1]] = ""
+        t.files[d[-1]] += content
+    
+    def readContentFromFile(self, filePath: str) -> str:
+        t = self.root
+        d = filePath.split("/")
+        
+        for i in range(1, len(d) - 1):
+            t = t.dirs[d[i]]
+        
+        return t.files[d[-1]]
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.ls(path)
+# obj.mkdir(path)
+# obj.addContentToFile(filePath,content)
+# param_4 = obj.readContentFromFile(filePath)
