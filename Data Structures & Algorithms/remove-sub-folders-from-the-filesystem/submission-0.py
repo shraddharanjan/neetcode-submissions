124b8c082cc1edcde05ac8119d98e79efcb33355
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-05-25 16:02:04 +0100

    Add: remove-sub-folders-from-the-filesystem - submission-0

---
+++ Data Structures & Algorithms/remove-sub-folders-from-the-filesystem/submission-0.py
@@ -0,0 +1,34 @@
+class Trie:
+    def __init__(self):
+        self.children = {}  # string -> Trie
+        self.end_of_folder = False
+
+    def add(self, path: str) -> None:
+        cur = self
+        for f in path.split("/"):
+            if f not in cur.children:
+                cur.children[f] = Trie()
+            cur = cur.children[f]
+        cur.end_of_folder = True
+
+    def prefix_search(self, path: str) -> bool:
+        cur = self
+        folders = path.split("/")
+        for i in range(len(folders) - 1):
+            cur = cur.children[folders[i]]
+            if cur.end_of_folder:
+                return True
+        return False
+
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        trie = Trie()
+        for f in folder:
+            trie.add(f)
+
+        res = []
+        for f in folder:
+            if not trie.prefix_search(f):
+                res.append(f)
+        return res
