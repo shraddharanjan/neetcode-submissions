f79e4acb180b8ca853ab4c1052683c0a19092527
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-06-27 20:30:54 +0100

    Add: zigzag-iterator - submission-0

---
+++ Data Structures & Algorithms/zigzag-iterator/submission-0.py
@@ -0,0 +1,31 @@
+class ZigzagIterator:
+    def __init__(self, v1: List[int], v2: List[int]):
+        self.vectors = [v1, v2]
+        self.queue = deque()
+        for index, vector in enumerate(self.vectors):
+            # <index_of_vector, index_of_element_to_output>
+            if len(vector) > 0:
+                self.queue.append((index, 0))
+
+    def next(self) -> int:
+
+        if self.queue:
+            vec_index, elem_index = self.queue.popleft()
+            next_elem_index = elem_index + 1
+            if next_elem_index < len(self.vectors[vec_index]):
+                # append the pointer for the next round
+                # if there are some elements left
+                self.queue.append((vec_index, next_elem_index))
+
+            return self.vectors[vec_index][elem_index]
+
+        # no more element to output
+        raise Exception
+
+    def hasNext(self) -> bool:
+        return len(self.queue) > 0
+        
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())
