f589a21c3ae07ba924aab6347bd207ccf93455bb
Shraddha Ranjan <87902824+shraddharanjan@users.noreply.github.com>
2026-06-27 20:33:21 +0100

    Add: design-a-leaderboard - submission-0

---
+++ Data Structures & Algorithms/design-a-leaderboard/submission-0.py
@@ -0,0 +1,31 @@
+class Leaderboard:
+
+    def __init__(self):
+        self.scores = {}
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.scores:
+            self.scores[playerId] = 0
+        self.scores[playerId] += score
+
+    def top(self, K: int) -> int:
+    
+        # This is a min-heap by default in Python.
+        heap = []
+        for x in self.scores.values():
+            heapq.heappush(heap, x)
+            if len(heap) > K:
+                heapq.heappop(heap)
+        res = 0
+        while heap:
+            res += heapq.heappop(heap)
+        return res
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
+
+# Your Leaderboard object will be instantiated and called as such:
+# obj = Leaderboard()
+# obj.addScore(playerId,score)
+# param_2 = obj.top(K)
+# obj.reset(playerId)
